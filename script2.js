69973565fab1d8937546d302ac91935953856806
Amram1997 <amramananyan9@gmail.com>
2021-12-02 21:18:55 +0400

    feat

--- script2.js
+++ script2.js
@@ -1,2 +1,8 @@
 /*2. Given two variables, which are the angles of a triangle. Find the third angle of the triangle. (Sum of
 the angles of a triangle equals 180 degrees). */
+
+function thriAngle(a,b) {
+    let sum = 180 - (a + b)
+    console.log(sum) 
+}
+thriAngle(30,30)
