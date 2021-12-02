70e71f8f1a55cf776349bb2568a649d6fd11f382
Amram1997 <amramananyan9@gmail.com>
2021-12-02 21:25:42 +0400

    feat

---
+++ script3.js
@@ -0,0 +1,12 @@
+/* 1. Given a positive integer. Bring the last digit of the number to the beginning. Print the new number. If
+the last digit of the inserted number is 0, number remains the same.
+ */
+
+// chstacvec verjin@ araj berel
+function reverseOne(a) {
+    let reverse1 = a + "";
+    let rev = reverse1.split("");
+    console.log(rev)
+   
+}
+reverseOne(367)
